2aa1c4c3ccc44487233e7368295dd334719a430a
alexnesov <alexandrenesovic@gmail.com>
2023-11-08 22:12:32 +0100

    js init

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from celery.result import AsyncResult
 from celery_worker.tasks import create_task
+import json
+from fastapi.websockets import WebSocket, WebSocketDisconnect  # Add these imports
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -10,7 +12,7 @@ templates = Jinja2Templates(directory="templates")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/create_task/")
+@app.post("/create_task/", response_model=dict)
 async def create_task_route(task_name: str = Form(...)):
     task = create_task.delay(task_name)
     return {"task_id": task.id}
@@ -19,6 +21,19 @@ async def create_task_route(task_name: str = Form(...)):
 async def get_task_status(task_id: str):
     task = AsyncResult(task_id, app=create_task.app)
     return {"task_status": task.status, "result": task.result}
+
+# WebSocket route for real-time updates
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            task_id = json.loads(data)["task_id"]
+            task_status = get_task_status(task_id)
+            await websocket.send_text(json.dumps(task_status))
+    except WebSocketDisconnect:
+        pass
 
 if __name__ == "__main__":
     import uvicorn
